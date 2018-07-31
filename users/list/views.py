4444f85a60dad5ee2283f76bea01f367d8643f7f
szpone <nikola.adamus@gmail.com>
2018-07-31 12:22:01 +0200

    Prepare template and view for listing users

--- users/list/views.py
+++ users/list/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import User
+
 
 # Create your views here.
+
+class UserListView(ListView):
+    model = User
+    queryset = User.objects.all()
