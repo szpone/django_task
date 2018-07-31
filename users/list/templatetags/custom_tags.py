eaa7022b455c84d76aa2c2a5fdb3841ceda520c2
szpone <nikola.adamus@gmail.com>
2018-07-31 14:06:38 +0200

    Add BizzFuzz custom tag, refactor custom_tags, display user list accrodingly to custom tags' '

--- users/list/templatetags/custom_tags.py
+++ users/list/templatetags/custom_tags.py
@@ -4,6 +4,19 @@ from datetime import date
 register = template.Library()
 
 
+user_status = {
+    "allowed": "Allowed",
+    "blocked": "Blocked"
+}
+
+number_status = {
+    "bizz": "Bizz",
+    "fuzz": "Fuzz",
+    "bizz_fuzz": "BizzFuzz",
+    "invalid": "Not valid for BizzFuzz",
+}
+
+
 def calculate_age(birthday):
     today = date.today()
     return today.year - birthday.year
@@ -12,10 +25,24 @@ def calculate_age(birthday):
 @register.simple_tag
 def status(user):
     if user.birthday is not None:
-        if calculate_age(user.birthday) <= 13:
-            return "blocked"
+        if calculate_age(user.birthday) < 13:
+            return user_status["blocked"]
         else:
-            return "allowed"
+            return user_status["allowed"]
+
+
+@register.simple_tag
+def bizz_fuzz(user):
+    number = user.random_number
+    if number % 3 == 0 and number % 5 == 0:
+        return number_status["bizz_fuzz"]
+    elif number % 3 == 0:
+        return number_status["bizz"]
+    elif number % 5 == 0:
+        return number_status["fuzz"]
+    else:
+        return number_status['invalid']
+
 
 
 
